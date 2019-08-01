77f2b3b12c66146e95c11b70e391ea92bf1df67b
Santiago Marino Vasquez <samava1410@hotmail.com>
2019-08-01 15:12:48 -0500

    Problema con las rutas

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+//Conexion con la BD Mongo
+connection = mongoose.createConnection('mongodb://localhost/Cine', { useNewUrlParser: true })
+    .then(db => console.log('Conectado a la BD Cine'))
+    .catch(err => console.log('Problemas de conexion de bases de datos' + err));
+
+//Configuracion de las vistas
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//Configuracion de las rutas
+const routes = require('./routes/routes');
+app.use('/', routes);
+
+//Iniciar servidor Express
+app.listen(app.set('port'), () => {
+    console.log('Servidor escuchando en :3000');
+});
