31e6b21f9e4772935a6560a85eb3a9a51b0cb2d7
Santiago Marino Vasquez <samava1410@hotmail.com>
2019-08-01 12:30:42 -0500

    Inicar servidor con Express

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+//Configuraciones
+app.set('port', 3000);
+
+//Rutas
+
+//Iniciar servidor
+server.listen(app.get('port'), function () {
+    console.log('Servidor escuchando en puerto 3000');
+})
